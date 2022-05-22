0b4581daa502ca834bb022f5b6e69ab897f4b441
poparicsara <poparic.ra60.2018@uns.ac.rs>
2022-05-22 19:42:01 +0200

    feat: connect riding school offers

---
+++ NationalEquestrianFederation/screens/AddRidingSchoolOffer.js
@@ -0,0 +1,84 @@
+import { StyleSheet, Button, TextInput, View, Text, ScrollView } from "react-native";
+import { globalStyles } from "../styles/global";
+import { Formik } from 'formik';
+
+export default function AddRidingSchoolOffer({ addOffer }) {
+
+    return (
+        <View>
+            <Formik
+                initialValues={{name: '', price: 0, startDate: '', endDate: '', description: '', horseClub: 1}}
+                onSubmit={(values, actions) => {
+                    actions.resetForm();
+                    addOffer(values);
+                }}>
+                {(props) => (
+                    <ScrollView>
+
+                        <Text style={styles.titleText}>Add riding school offer</Text>
+
+                        <TextInput 
+                            style={globalStyles.input} 
+                            placeholder='Name'
+                            onChangeText={props.handleChange('name')}
+                            value={props.values.name}
+                            onBlur={props.handleBlur('name')}
+                        />
+                        <Text style={globalStyles.errorText}>{props.touched.name && props.errors.name}</Text>
+
+                        <TextInput 
+                            style={globalStyles.input} 
+                            placeholder='Price'
+                            keyboardType="numeric"
+                            onChangeText={props.handleChange('price')}
+                            value={props.values.price}
+                            onBlur={props.handleBlur('price')}
+                        />
+                        <Text style={globalStyles.errorText}>{props.touched.price && props.errors.price}</Text>
+
+                        <TextInput 
+                            style={globalStyles.input} 
+                            placeholder='Start date'
+                            onChangeText={props.handleChange('startDate')}
+                            value={props.values.startDate}
+                            onBlur={props.handleBlur('startDate')}
+                        />
+                        <Text style={globalStyles.errorText}>{props.touched.startDate && props.errors.startDate}</Text>
+
+                        <TextInput 
+                            style={globalStyles.input} 
+                            placeholder='End date'
+                            onChangeText={props.handleChange('endDate')}
+                            value={props.values.endDate}
+                            onBlur={props.handleBlur('endDate')}
+                        />
+                        <Text style={globalStyles.errorText}>{props.touched.endDate && props.errors.endDate}</Text>
+
+                        <TextInput 
+                            style={globalStyles.input} 
+                            placeholder='Description'
+                            onChangeText={props.handleChange('description')}
+                            value={props.values.description}
+                            onBlur={props.handleBlur('description')}
+                        />
+                        <Text style={globalStyles.errorText}>{props.touched.description && props.errors.description}</Text>
+
+                        <Button title="Submit" onPress={props.handleSubmit} />
+
+                    </ScrollView>
+                )}
+            </Formik>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    titleText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        alignSelf: 'center',
+        marginBottom: 10
+    },
+})
--- NationalEquestrianFederation/screens/RidingSchoolOffers.js
+++ NationalEquestrianFederation/screens/RidingSchoolOffers.js
@@ -1,14 +1,20 @@
-import { Text, ImageBackground, FlatList, StyleSheet, View } from 'react-native';
+import { Text, ImageBackground, FlatList, StyleSheet, View, Modal, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Card from '../shared/card';
 import { globalStyles } from '../styles/global';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { MaterialIcons } from '@expo/vector-icons';
+import AddRidingSchoolOffer from './AddRidingSchoolOffer';
+import EditRidingSchoolOffer from './EditRidingSchoolOffer';
 
 export default function RidingSchoolOffers({ navigation }) {
 
     const serverUrl = "http://10.0.2.2:8080";
 
-    const [offers, setOffers] = useState([])
+    const [addModalOpen, setAddModalOpen] = useState(false);
+    const [editModalOpen, setEditModalOpen] = useState(false);
+    const [offers, setOffers] = useState([]);
+    const [editingOffer, setEdditingOffer] = useState({});
 
     useEffect(() => {
         getOffers();
@@ -21,10 +27,73 @@ export default function RidingSchoolOffers({ navigation }) {
             })
     }
 
+    const deleteOffer = (id) => {
+        axios.delete(serverUrl + "/ridingSchoolOffers/" + id)
+            .then(response => {
+                getOffers();
+            })
+    }
+
+    const addOffer = (offer) => {
+        axios.post(serverUrl + "/ridingSchoolOffers", offer)
+            .then(response => {
+                setAddModalOpen(false);
+                getOffers();
+            })
+    }
+
+    const editOffer = (offer) => {
+        axios.put(serverUrl + "/ridingSchoolOffers", offer)
+            .then(response => {
+                setEditModalOpen(false);
+                getOffers();
+            })
+    }
+
+    const editForm = (offer) => {
+        setEdditingOffer(offer);
+        setEditModalOpen(true);
+    }
+
     return (
         <ImageBackground source={require('../assets/background.jpg')} style={globalStyles.container} >
+
+            <Modal visible={addModalOpen}>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={globalStyles.modalContent}>
+                        <MaterialIcons 
+                            name='close' 
+                            size={24} 
+                            style={{...globalStyles.closeButton, ...globalStyles.modalClose}}
+                            onPress={() => setAddModalOpen(false)} />
+                        <AddRidingSchoolOffer addOffer={addOffer}  />
+                    </View>
+                </TouchableWithoutFeedback>
+            </Modal>
+
+            <MaterialIcons 
+                name='add' 
+                size={24} 
+                style={globalStyles.addButton}
+                color="rgba(252, 252, 252, 0.8)"
+                onPress={() => setAddModalOpen(true)} />
+
             <FlatList data={offers} renderItem={({ item }) => (
                 <Card>
+
+                    <Modal visible={editModalOpen}>
+                        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                            <View style={globalStyles.modalContent}>
+                                <MaterialIcons 
+                                    name='close' 
+                                    size={24} 
+                                    style={{...globalStyles.closeButton, ...globalStyles.modalClose}}
+                                    onPress={() => setEditModalOpen(false)} />
+                                <EditRidingSchoolOffer offer={editingOffer} editOffer={editOffer}  />
+                            </View>
+                        </TouchableWithoutFeedback>
+                    </Modal>
+
                     <Text style={globalStyles.titleText}>{item.name}</Text>
                     <View style={styles.content}>
                         <Text style={styles.label}>Start</Text>
@@ -36,6 +105,21 @@ export default function RidingSchoolOffers({ navigation }) {
                         <Text style={styles.label}>Price</Text>
                         <Text>{item.price}e</Text>
                     </View>
+
+                    <View style={styles.buttons}>
+                        <MaterialIcons 
+                            name='delete' 
+                            size={24} 
+                            style={styles.deleteButton} 
+                            onPress={() => deleteOffer(item.id)} />
+
+                        <MaterialIcons 
+                            name='edit' 
+                            size={24} 
+                            style={styles.deleteButton} 
+                            onPress={() => editForm(item)} />
+                    </View>
+
                 </Card>
             )} style={styles.cards}/>
         </ImageBackground>
@@ -44,7 +128,7 @@ export default function RidingSchoolOffers({ navigation }) {
 
 const styles = StyleSheet.create({
     cards: {
-        marginTop: 15
+        marginTop: 5
     },
     name: {
         flexDirection: 'row'
@@ -65,5 +149,12 @@ const styles = StyleSheet.create({
         fontSize: 15,
         fontWeight: 'bold'
     },
-
+    buttons: {
+        flexDirection: 'row',
+        alignSelf: 'flex-end'
+    },  
+    deleteButton: {
+        alignSelf: 'flex-end',
+        marginLeft: 10
+    }
 })
