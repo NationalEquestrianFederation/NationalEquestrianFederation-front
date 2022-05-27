d3fbddcecf7061982765ae476dbafb55d09e548a
poparicsara <poparic.ra60.2018@uns.ac.rs>
2022-05-27 08:04:40 +0200

    feat: add competition

---
+++ NationalEquestrianFederation/screens/AddCompetition.js
@@ -0,0 +1,123 @@
+import { StyleSheet, Button, TextInput, View, Text, ScrollView, TouchableOpacity } from "react-native";
+import { globalStyles } from "../styles/global";
+import { useState } from "react";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Picker } from '@react-native-picker/picker';
+
+export default function AddCompetition({ addCompetition }) {
+
+    const [name, setName] = useState('');
+    const [startDate, setStartDate] = useState(new Date());
+    const [openStartDate, setOpenStartDate] = useState(false);
+    const [startDateText, setStartDateText] = useState(new Date().getDate() + '/' + (new Date().getMonth() + 1) + '/' + new Date().getFullYear());
+    const [endDate, setEndDate] = useState(new Date());
+    const [openEndDate, setOpenEndDate] = useState(false);
+    const [endDateText, setEndDateText] = useState(new Date().getDate() + '/' + (new Date().getMonth() + 1) + '/' + new Date().getFullYear());
+    const [location, setLocation] = useState('');
+    const [discipline, setDiscipline] = useState('');
+
+    const addCompetitionHandler = () => {
+        var competition = {
+            name: name,
+            startDate: startDateText,
+            endDate: endDateText,
+            location: location,
+            discipline: discipline
+        }
+        addCompetition(competition);
+    }
+
+    const startDateChange = (event, selectedDate) => {
+        const currentDate = selectedDate || startDate;
+        setStartDate(currentDate);
+        setOpenStartDate(false);
+
+        var tempDate = new Date(currentDate);
+        let fDate = tempDate.getDate() + '/' + (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear();
+        setStartDateText(fDate);
+    }
+
+    const endDateChange = (event, selectedDate) => {
+        const currentDate = selectedDate || endDate;
+        setEndDate(currentDate);
+        setOpenEndDate(false);
+
+        var tempDate = new Date(currentDate);
+        let fDate = tempDate.getDate() + '/' + (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear();
+        setEndDateText(fDate);
+    }
+
+    return (
+        <View>
+            <ScrollView>
+                <Text style={styles.titleText}>Add competition</Text>
+
+                <TextInput 
+                    style={globalStyles.input} 
+                    placeholder='Name'
+                    onChangeText={(value) => setName(value)}
+                    value={name}
+                />
+                <Text></Text>
+
+                {openStartDate && (<DateTimePicker
+                    value={startDate}
+                    onChange={startDateChange}
+                    dateFormat="day month year"
+                    minimumDate={new Date()}
+                    />
+                )}
+
+                <TouchableOpacity style={globalStyles.dateButton} onPress={() => setOpenStartDate(true)}>
+                    <Text style={globalStyles.dateButtonText}>Start date - {startDateText}</Text>
+                </TouchableOpacity>
+                <Text></Text>
+
+                {openEndDate && (<DateTimePicker
+                    value={endDate}
+                    onChange={endDateChange}
+                    dateFormat="day month year"
+                    minimumDate={new Date()}
+                    />
+                )}
+
+                <TouchableOpacity style={globalStyles.dateButton} onPress={() => setOpenEndDate(true)}>
+                    <Text style={globalStyles.dateButtonText}>End date - {endDateText}</Text>
+                </TouchableOpacity>
+                <Text></Text>
+                
+                <TextInput 
+                    style={globalStyles.input} 
+                    placeholder='Location'
+                    onChangeText={(value) => setLocation(value)}
+                    value={location}
+                />
+                <Text></Text>
+
+                <Picker 
+                    selectedValue={discipline}
+                    onValueChange={value => setDiscipline(value)}>
+                    <Picker.Item label="Jumping" value="jumping" />
+                    <Picker.Item label="Dressage" value="dressage" />
+                    <Picker.Item label="Eventing" value="eventing" />
+                </Picker>
+                <Text></Text>
+
+                <Button title="Submit" onPress={addCompetitionHandler} />
+
+            </ScrollView>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    titleText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        alignSelf: 'center',
+        marginBottom: 10
+    },
+    
+})
--- NationalEquestrianFederation/screens/CompetitionCalendar.js
+++ NationalEquestrianFederation/screens/CompetitionCalendar.js
@@ -1,37 +1,94 @@
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, Modal, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { Calendar, CaledarList, Agenda } from 'react-native-calendars';
 import { globalStyles } from '../styles/global';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import AddCompetition from './AddCompetition';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function CompetitionCalendar({ navigation }) {
+
+    const serverUrl = "http://10.0.2.2:8080";
+
+    const [competitions, setCompetitions] = useState([]);
+    const [dates, setDates] = useState([]);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [competitionsInDay, setCompetitionsInDay] = useState([]);
+    
+
+    useEffect(() => {
+        getCompetitions();
+    }, [])
+
+    const getCompetitions = () => {
+        axios.get(serverUrl + "/competitions")
+            .then(response => {
+                setCompetitions(response.data);
+            })
+    }
+
+    const getMarkedDates = () => {
+        return {'2022-05-19': {periods: [{startingDay: true, endingDay: true, color: 'blur'}]}}
+    }
 
     const datePressHandler = (day) => {
         console.log(day.dateString)
         navigation.navigate('ChoosenDate', day)
     }
 
+    const addCompetition = (competition) => {
+        axios.post(serverUrl + "/competitions", competition)
+            .then(response => {
+                setModalOpen(false);
+                getCompetitions();
+            })
+    }
+
     return (
         <ImageBackground source={require('../assets/background.jpg')} style={globalStyles.container} >
             <View style={globalStyles.container}>
-                <Calendar style={styles.calendar} 
+
+            <Modal visible={modalOpen}>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={globalStyles.modalContent}>
+                        <MaterialIcons 
+                            name='close' 
+                            size={24} 
+                            style={{...globalStyles.closeButton, ...globalStyles.modalClose}}
+                            onPress={() => setModalOpen(false)} />
+                        <AddCompetition addCompetition={addCompetition}  />
+                    </View>
+                </TouchableWithoutFeedback>
+            </Modal>
+
+            <MaterialIcons 
+                name='add' 
+                size={24} 
+                style={globalStyles.addButton}
+                color="rgba(252, 252, 252, 0.8)"
+                onPress={() => setModalOpen(true)} />
+
+            <Calendar style={styles.calendar}
                 markingType="multi-period"
                 markedDates={{
-                    '2022-05-14': {
-                    periods: [
-                        {startingDay: false, endingDay: true, color: '#5f9ea0'},
-                        {startingDay: false, endingDay: true, color: '#ffa500'},
-                        {startingDay: true, endingDay: false, color: '#f0e68c'}
-                    ]
+                    '2022-05-19': {
+                        periods: [
+                            {startingDay: true, endingDay: false, color: 'blue'},
+                            {startingDay: true, endingDay: false, color: 'red'}
+                        ]
                     },
-                    '2022-05-15': {
-                    periods: [
-                        {startingDay: true, endingDay: false, color: '#ffa500'},
-                        {color: 'transparent'},
-                        {startingDay: false, endingDay: false, color: '#f0e68c'}
-                    ]
+                    '2022-05-20': {
+                        periods: [
+                            {startingDay: false, endingDay: true, color: 'blue'},
+                            {startingDay: false, endingDay: true, color: 'red'}
+                        ]
+                    },
+                    '2022-05-30': {
+                        periods: []
                     }
                 }}
-                onDayPress={(day) => datePressHandler(day)}
-                />
+            />
+
             </View>
         </ImageBackground>
     )
